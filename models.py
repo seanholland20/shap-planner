3060e6f1fbf7c64b4543f00bc2c0418b423266f6
seanholland20 <47987398+seanholland20@users.noreply.github.com>
2024-09-30 19:21:42 -0400

    Create models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    location = db.Column(db.String(200), nullable=False)
+    attendees = db.Column(db.Integer)
